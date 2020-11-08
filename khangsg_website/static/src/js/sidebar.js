3c5e8cb140ec10f94317119ce269a39f4007ffc9
Khangntd <khangnguyen16021990@gmail.com>
2020-11-08 18:11:53 +0700

    Change module name

--- khangsg_website/static/src/js/sidebar.js
+++
@@ -1,7 +0,0 @@
-odoo.define('khangsg_website.cafe_shop_sidebar', function (require) {
-    $(document).ready(function() {
-        if (window.location.pathname == '/'){
-            $('.hamburger_menus').hide();
-        }
-    });
-});
